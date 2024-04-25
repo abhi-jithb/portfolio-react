ed350b34c5d7bdf1ccd465d5a78c43476521603e
Abhijith B <babhijith58@gmail.com>
2024-04-25 09:39:10 +0000

    working on project page

---
+++ portfolio/src/components/HeroProject.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./HeroProjectStyles.css"
+
+const HeroProject = () => {
+    return (
+        <div className='hero-img'>
+            <div className='heading'>
+                <h1>Projects</h1>
+                <p>My most recent projects are showcased below</p>
+            </div>
+            
+        </div>
+    )
+}
+export default HeroProject
--- portfolio/src/routes/Project.js
+++ portfolio/src/routes/Project.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer.js'
+import HeroProject from '../components/HeroProject.js'
 
 
 const Project = () => {
     return (
         <>
             <Navbar />
-
+            <HeroProject />
             <Footer />
         </>
     )
