6fe5d7013fc53e6383f5b193bf9d771e6106abb0
Abhijith B <babhijith58@gmail.com>
2024-04-25 08:21:35 +0000

    footer section added

--- portfolio/src/routes/Home.js
+++ portfolio/src/routes/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import HeroImg from '../components/HeroImg.js'
+import Footer from '../components/Footer.js'
 
 const Home = () => {
     return (
         <>
             <Navbar />
             <HeroImg />
+            <Footer />
         </>
 
     )
