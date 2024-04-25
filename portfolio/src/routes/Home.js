f0f32f3c2d0ab34681a482c3994a231ca88a585f
Abhijith B <babhijith58@gmail.com>
2024-04-25 04:53:46 +0000

    hero section added

--- portfolio/src/routes/Home.js
+++ portfolio/src/routes/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import HeroImg from '../components/HeroImg.js'
 
 const Home = () => {
     return (
         <>
             <Navbar />
+            <HeroImg />
         </>
 
     )
