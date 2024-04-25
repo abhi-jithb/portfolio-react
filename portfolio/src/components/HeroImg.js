e179b0c0a5e4daa6b6d8140c6f57d8b2e55769b9
Abhijith B <babhijith58@gmail.com>
2024-04-25 05:40:15 +0000

    files added

--- portfolio/src/components/HeroImg.js
+++ portfolio/src/components/HeroImg.js
@@ -1,13 +1,22 @@
-import "./HeroImg.css"
+import "./HeroImg.css";
+import React from "react";
 
-import React from "react"
-
+import IntroImg from "../assets/intro-img.jpg";
+import { Link } from "react-router-dom"
 const HeroImg = () => {
     return (
         <>
             <div className="hero">
                 <div className="mask">
-
+                    <img className="intro-img" src={IntroImg} alt="intro image" />
+                </div>
+                <div className="content">
+                    <p>HI, I'M A FREELANCER.</p>
+                    <h1>React Developer.</h1>
+                    <div>
+                        <Link to="/projects" className="btn">Projects</Link>
+                        <Link to="/contact" className="btn-light">Contac</Link>
+                    </div>
                 </div>
             </div>
         </>
