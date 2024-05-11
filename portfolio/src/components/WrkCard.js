c5fb0e03edb08c0d01fac9f470b04b677741c84c
Abhijith <babhijth58@gmail.com>
2024-05-11 23:21:01 +0530

    projects page added

---
+++ portfolio/src/components/WrkCard.js
@@ -0,0 +1,29 @@
+import "./WrkCardStyles.css"
+
+import React from 'react';
+import pro1 from '../assets/networking.jpg'
+import { NavLink } from "react-router-dom";
+
+const WrkCard = () => {
+  return (
+    <div className="work-container">
+        <h1 className="project-heading">Projects</h1>
+        <div className="project-container">
+            <div className="project-card">
+                <img src={pro1} alt="project-1" width={"100%"} height={"60%"}/>
+                 <h2 className="project-title">Project Title</h2>
+                 <div className="project-details">
+                    <p>this is the project description</p>
+                    <div className="project-btns">
+                    <NavLink to="#" className="btn">View</NavLink>
+
+                        <NavLink to="#" className="btn">Source</NavLink>
+                    </div>
+                 </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default WrkCard
--- portfolio/src/routes/Project.js
+++ portfolio/src/routes/Project.js
@@ -5,6 +5,7 @@ import Footer from '../components/Footer.js'
 // import HeroImg2 from '../components/heroImage/HeroImg2.js'
 import HeroImg2 from '../components/HeroImg2.js'
 import PricingCard from '../components/PricingCard.js'
+import WrkCard from '../components/WrkCard.js'
 
 
 const Project = () => {
@@ -12,6 +13,7 @@ const Project = () => {
         <>
             <Navbar />
             <HeroImg2 heading="Projects." text="Some the latest Projects"/>
+            <WrkCard/>
             <PricingCard/>
             <Footer />
         </>
