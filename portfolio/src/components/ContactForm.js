45c94a9bc060adf4f317b3797c95821f39c518c6
Abhijith B <babhijith58@gmail.com>
2024-05-16 07:09:46 +0000

    code updated

--- portfolio/src/components/ContactForm.js
+++ portfolio/src/components/ContactForm.js
@@ -2,25 +2,26 @@ import "./ContactStyles.css"
 
 import React from 'react';
 
+const ContactForm = () => {
     return (
         <div className="form">
             <form onSubmit={handleSubmit}>
                 <label>Name:</label>
-                <input type="text"} />
+                <input type="text" />
 
                 <label>Email:</label>
                 <input type="email" />
 
                 <label>Subject:</label>
-                <input type="text"/>
+                <input type="text" />
 
                 <label>Message:</label>
-                <textarea rows="6" placeholder="Please type your message here..."}></textarea>
+                <textarea rows="6" placeholder="Please type your message here..."></textarea>
 
                 <button className="btn" type="submit">Submit</button>
             </form>
-        </div>
+        </div >
     );
-};
+}
 
 export default ContactForm;
