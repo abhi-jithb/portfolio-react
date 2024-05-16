8e739497f41bc1424c848aa9dfd15bf6005b8d76
Abhijith B <babhijith58@gmail.com>
2024-05-16 12:23:52 +0530

    Update ContactForm.js

--- portfolio/src/components/ContactForm.js
+++ portfolio/src/components/ContactForm.js
@@ -1,41 +1,21 @@
-import React, { useState } from 'react';
-import { GoogleSpreadsheet } from 'google-spreadsheet';
 import "./ContactStyles.css"
 
-const ContactForm = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [subject, setSubject] = useState('');
-    const [message, setMessage] = useState('');
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        const doc = new GoogleSpreadsheet('1l6sHTQfM2w6Fn6jzb8kz5CB3l9fIJZDnK2vzfA8duBA');
-        doc.useApiKey('AIzaSyADaQa9e9myzPnAGR8KC4mNThIuOiLkboA');
-
-        await doc.loadInfo();
-        const sheet = doc.sheetsByIndex[0];
-
-        await sheet.addRow({ Name: name, Email: email, Subject: subject, Message: message });
-
-        console.log('Data added to Google Sheets successfully!');
-    };
+import React from 'react';
 
     return (
         <div className="form">
             <form onSubmit={handleSubmit}>
                 <label>Name:</label>
-                <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                <input type="text"} />
 
                 <label>Email:</label>
-                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input type="email" />
 
                 <label>Subject:</label>
-                <input type="text" value={subject} onChange={(e) => setSubject(e.target.value)} />
+                <input type="text"/>
 
                 <label>Message:</label>
-                <textarea rows="6" placeholder="Please type your message here..." value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+                <textarea rows="6" placeholder="Please type your message here..."}></textarea>
 
                 <button className="btn" type="submit">Submit</button>
             </form>
